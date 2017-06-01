07269afbf54d157f27bb8a8ec1c51e72cf1a17f1
nappannda <qwe159753qwee@gmail.com>
2017-06-01 15:39:37 +0900

    init commit

---
+++ GithubRepositorySearchApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubRepositorySearchApp
+//
+//  Created by 上原和輝 on 2017/06/01.
+//  Copyright © 2017年 nappannda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
