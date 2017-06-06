b8a26c3ff50b383c3375a7baadbadd58e437ed75
nappannda <qwe159753qwee@gmail.com>
2017-06-06 16:26:10 +0900

    add sort and order picker

--- GithubRepositorySearchApp/ViewController.swift
+++ GithubRepositorySearchApp/ViewController.swift
@@ -11,24 +11,38 @@ import APIKit
 import RxSwift
 import RxCocoa
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
-    
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+  
+    @IBOutlet weak var sortPickerView: UIPickerView!
+    @IBOutlet weak var orderPickerView: UIPickerView!
     @IBOutlet weak var repositoriesTableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
-    var searchText = ""
     
+    let sortList = ["", "stars", "forks", "updated"]
+    let orderList = ["desc", "asc"]
     let disposeBag = DisposeBag()
     var repositories = GithubRepositories()
+    var searchText = ""
+    var sortText = ""
+    var orderText = "desc"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         repositoriesTableView.delegate = self
         repositoriesTableView.dataSource = self
+        sortPickerView.delegate = self
+        sortPickerView.dataSource = self
+        sortPickerView.tag = 1
+        orderPickerView.delegate = self
+        orderPickerView.dataSource = self
+        orderPickerView.tag = 2
         
         
-        searchBar.rx.text
+        searchBar.rx.text.orEmpty
+            .throttle(1.0, scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
             .subscribe(onNext: { [unowned self] q in
-                    self.searchText = q!
+                    self.searchText = q
                     self.searchRepository()
                 },
                 onError: { err in
@@ -42,7 +56,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func searchRepository() {
-        let request = FetchRepositoriesRequest.init(query: self.searchText, sort: "", order: "")
+        let request = FetchRepositoriesRequest.init(query: self.searchText, sort: sortText, order: orderText)
         Session.rx_response(request: request)
             .subscribe(
                 onNext: { GithubRepositories in
@@ -72,6 +86,37 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         return cell
     }
+    
+    @available(iOS 2.0, *)
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 1 {
+            return sortList.count
+        } else {
+            return orderList.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int)-> String? {
+        if pickerView.tag == 1 {
+            return sortList[row]
+        } else {
+            return orderList[row]
+        }
+    }
 
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView.tag == 1 {
+            sortText = sortList[row]
+        } else {
+            orderText = orderList[row]
+        }
+        searchRepository()
+    }
+
+    
 }
 
